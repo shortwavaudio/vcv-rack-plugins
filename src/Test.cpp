2e4dc4fe5d3281910c224330f274df465567f5c1
Stephane P. Pericat <shortwavaudio@Stephanes-MacBook-Pro.local>
2022-05-07 16:47:31 -0500

    noise gen

--- src/Test.cpp
+++ src/Test.cpp
@@ -2,7 +2,7 @@
 
 void Test::process(const ProcessArgs &args)
 {
-
+    outputs[AUDIO_OUTPUT].setVoltage(random::uniform() * 5.0f);
 }
 
 Model *modelTest = createModel<Test, TestWidget>("Test");
--- src/Test.hpp
+++ src/Test.hpp
@@ -16,6 +16,7 @@ struct Test : Module
 
   enum OutputIds
   {
+    AUDIO_OUTPUT,
     NUM_OUTPUTS
   };
 
@@ -40,5 +41,7 @@ struct TestWidget : ModuleWidget
 
     addChild(createWidget<ScrewSilver>(Vec(0, 0)));
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 1 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+    addOutput(createOutput<PJ301MPort>(Vec(10.f, 300.f), module, Test::AUDIO_OUTPUT));
   }
 };
