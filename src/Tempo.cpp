6ce99613348d3e1fcb28e3c6e2841ab9acef5bc3
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-11-19 17:47:40 -0600

    adjust bpm

--- src/Tempo.cpp
+++ src/Tempo.cpp
@@ -1,14 +1,49 @@
 #include "Tempo.hpp"
 
+float Tempo::getPhase()
+{
+  return timer.getTime() * (10.f / frequency);
+}
+
+bool Tempo::hasPeaked()
+{
+  return timer.getTime() > frequency;
+}
+
+void Tempo::incrementTimer(float deltaTime)
+{
+  timer.process(deltaTime);
+}
+
+void Tempo::resetTimer()
+{
+  timer.reset();
+}
+
+void Tempo::setBpm(float bpmValue)
+{
+  bpm = bpmValue;
+}
+
+void Tempo::setFrequency()
+{
+  frequency = 60.f / bpm;
+}
+
 void Tempo::process(const ProcessArgs &args)
 {
-  timer.process(args.sampleTime);
-
-  if(timer.getTime() > 1.f) {
-    timer.reset();
+  if(params[BPM_PARAM].getValue() != bpm) {
+    setBpm(params[BPM_PARAM].getValue());
+    setFrequency();
   }
 
-  outputs[TRIGGER_OUTPUT].setVoltage(timer.getTime());
+  incrementTimer(args.sampleTime);
+
+  if(hasPeaked()) {
+    resetTimer();
+  }
+
+  outputs[TRIGGER_OUTPUT].setVoltage(getPhase());
 }
 
 Model *modelTempo = createModel<Tempo, TempoWidget>("Tempo");
--- src/Tempo.hpp
+++ src/Tempo.hpp
@@ -6,6 +6,7 @@ struct Tempo : Module
 {
   enum ParamIds
   {
+    BPM_PARAM,
     NUM_PARAMS
   };
 
@@ -27,11 +28,23 @@ struct Tempo : Module
 
   Tempo() {
     config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+    configParam(BPM_PARAM, 0.f, 240.f, bpm);
   }
 
   void process(const ProcessArgs &args) override;
 
+  float bpm = 120.f;
+  float frequency = 0.5f;
+
   dsp::Timer timer;
+
+  float getPhase();
+  bool hasPeaked();
+  void incrementTimer(float deltaTime);
+  void resetTimer();
+
+  void setBpm(float bpmValue);
+  void setFrequency();
 };
 
 struct TempoWidget : ModuleWidget
@@ -43,6 +56,8 @@ struct TempoWidget : ModuleWidget
 
     addChild(createWidget<ScrewSilver>(Vec(0, 0)));
     addChild(createWidget<ScrewSilver>(Vec(box.size.x - 1 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+    addParam(createParam<RoundSmallBlackKnob>(Vec(10.f, 50.f), module, Tempo::BPM_PARAM));
 
     addOutput(createOutput<PJ301MPort>(Vec(10.f, 310.f), module, Tempo::TRIGGER_OUTPUT));
   }
